041bfe5f343614cd3d4e26ececd79529b4a95d04
Serp <manu.serpette74@gmail.com>
2023-03-22 14:37:22 -0400

    Part3 Done

--- src/flow.py
+++ src/flow.py
@@ -208,61 +208,123 @@ def nr_nd(dicodef, dicoref):
             defnonref.append([x, dicoref[x][0]])
     return refnondef, defnonref
 
+
+def prepare_query_arg_node(ast, cfg, node):
+    arg = []
+    rhs = ast.get_children(node)[1]
+    for child in ast.get_children(rhs):
+        child_n = ast.get_children(child)[0]
+        if ast.get_type(child_n) == "Variable":
+            arg.append([cfg.get_node_cfg_ptr(child_n), ast.get_image(child_n)])
+
+    return arg
+
+
+def flatten(l):
+    return [item for sublist in l for item in sublist]
+
+
     # algo
 if __name__ == "__main__":
     cfgreader = CFGReader()
+    astreader = ASTReader()
 
-    cfg = cfgreader.read_cfg("../tp4/part_1/test.php.cfg.json")
-    flow = flow_class()
-    adef, bdef = flow.pos_reaching_def(cfg)
+    # cfg = cfgreader.read_cfg("../tp4/part_1/test.php.cfg.json")
+    # flow = flow_class()
+    # adef, bdef = flow.pos_reaching_def(cfg)
 
-    aref, bref, ref, definition = flow.pos_reachable_ref(cfg)
-    print("\ntest:")
-    print_defref_set(definition, ref, aref, adef)
+    # aref, bref, ref, definition = flow.pos_reachable_ref(cfg)
+    # print("\ntest:")
+    # print_defref_set(definition, ref, aref, adef)
 
-    print("\nwordcount:")
-    cfg = cfgreader.read_cfg("../tp4/part_1/wordcount.php.cfg.json")
-    flow = flow_class()
-    adef, bdef = flow.pos_reaching_def(cfg)
-    aref, bref, ref, definition = flow.pos_reachable_ref(cfg)
-    print_defref_set(definition, ref, aref, adef)
+    # print("\nwordcount:")
+    # cfg = cfgreader.read_cfg("../tp4/part_1/wordcount.php.cfg.json")
+    # flow = flow_class()
+    # adef, bdef = flow.pos_reaching_def(cfg)
+    # aref, bref, ref, definition = flow.pos_reachable_ref(cfg)
+    # print_defref_set(definition, ref, aref, adef)
 
-    # part 2.1
-    print("\npart 2.1")
-    print("\nfile1:")
-    cfg = cfgreader.read_cfg("../tp4/part_2/file1.php.cfg.json")
-    flow = flow_class()
-    adef, bdef = flow.pos_reaching_def(cfg)
-    aref, bref, ref, definition = flow.pos_reachable_ref(cfg)
-    dicodef, dicoref = print_defref_set(
-        definition, ref, aref, adef, verbose=False)
+    # # part 2.1
+    # print("\npart 2.1")
+    # print("\nfile1:")
+    # cfg = cfgreader.read_cfg("../tp4/part_2/file1.php.cfg.json")
+    # flow = flow_class()
+    # adef, bdef = flow.pos_reaching_def(cfg)
+    # aref, bref, ref, definition = flow.pos_reachable_ref(cfg)
+    # dicodef, dicoref = print_defref_set(
+    #     definition, ref, aref, adef, verbose=False)
 
-    refnondef, defnonref = nr_nd(dicodef, dicoref)
-    print("\nref non def ", refnondef)
-    # On detecte file name car on considere pas comme une defintion le parametre de la fonction
-    print("\ndef non ref ", defnonref)
+    # refnondef, defnonref = nr_nd(dicodef, dicoref)
+    # print("\nref non def ", refnondef)
+    # # On detecte file name car on considere pas comme une defintion le parametre de la fonction
+    # print("\ndef non ref ", defnonref)
 
-    print("\nfile2:")
-    cfg = cfgreader.read_cfg("../tp4/part_2/file2.php.cfg.json")
-    flow = flow_class()
-    adef, bdef = flow.pos_reaching_def(cfg)
-    aref, bref, ref, definition = flow.pos_reachable_ref(cfg)
-    dicodef, dicoref = print_defref_set(
-        definition, ref, aref, adef, verbose=False)
-    refnondef, defnonref = nr_nd(dicodef, dicoref)
-    print("\nref non def ", refnondef)
-    print("\ndef non ref ", defnonref)
+    # print("\nfile2:")
+    # cfg = cfgreader.read_cfg("../tp4/part_2/file2.php.cfg.json")
+    # flow = flow_class()
+    # adef, bdef = flow.pos_reaching_def(cfg)
+    # aref, bref, ref, definition = flow.pos_reachable_ref(cfg)
+    # dicodef, dicoref = print_defref_set(
+    #     definition, ref, aref, adef, verbose=False)
+    # refnondef, defnonref = nr_nd(dicodef, dicoref)
+    # print("\nref non def ", refnondef)
+    # print("\ndef non ref ", defnonref)
 
-    print("\nfile3:")
-    cfg = cfgreader.read_cfg("../tp4/part_2/file3.php.cfg.json")
-    flow = flow_class()
-    adef, bdef = flow.pos_reaching_def(cfg)
-    aref, bref, ref, definition = flow.pos_reachable_ref(cfg)
-    dicodef, dicoref = print_defref_set(
-        definition, ref, aref, adef, verbose=False)
-    refnondef, defnonref = nr_nd(dicodef, dicoref)
-    print("\nref non def ", refnondef)
-    print("\ndef non ref ", defnonref)
+    # print("\nfile3:")
+    # cfg = cfgreader.read_cfg("../tp4/part_2/file3.php.cfg.json")
+    # flow = flow_class()
+    # adef, bdef = flow.pos_reaching_def(cfg)
+    # aref, bref, ref, definition = flow.pos_reachable_ref(cfg)
+    # dicodef, dicoref = print_defref_set(
+    #     definition, ref, aref, adef, verbose=False)
+    # refnondef, defnonref = nr_nd(dicodef, dicoref)
+    # print("\nref non def ", refnondef)
+    # print("\ndef non ref ", defnonref)
 
     # part 2.2
-    print("\npart 2.2")
+    print("\npart 3")
+    cfg = cfgreader.read_cfg("../tp4/part_3/file.php.cfg.json")
+    ast = astreader.read_ast("../tp4/part_3/file.php.ast.json")
+    flow = flow_class()
+    adef, bdef = flow.pos_reaching_def(cfg)
+    aref, bref, ref, definition = flow.pos_reachable_ref(cfg)
+    dicodef, dicoref = print_defref_set(
+        definition, ref, aref, adef, verbose=False)
+
+    total_ref_to_check = []
+    for node in ast.get_node_ids():
+        if ast.get_type(node) == "MethodCall" and ast.get_image(node) == "prepare_query":
+
+            ref_to_check = prepare_query_arg_node(ast, cfg, node)
+            total_ref_to_check.append(ref_to_check)
+    total_ref_to_check = flatten(total_ref_to_check)
+    print("\nref to check", total_ref_to_check)
+
+    unclean = []
+    for ref in total_ref_to_check:
+        flag = False
+        def_reaching = dicoref[ref[0]][1:]
+        print("\ndef reaching for ref ", ref, " : ", def_reaching)
+
+        for defin in def_reaching:
+            flag2 = True
+            ast_node = cfg.get_node_ast_ptr(defin)
+            node_parent = ast.get_parents(ast_node)[0]
+
+            if ast.get_type(node_parent) == "BinOP" and ast.get_image(node_parent) == "=":
+                rhs = ast.get_children(node_parent)[1]
+                if ast.get_type(rhs) == "FunctionCall" and ast.get_image(rhs) == "filter_var":
+                    print("Function call: filter_var for def ",
+                          def_reaching[0])
+                    print("Var is filtered")
+                    flag2 = False
+            if flag2:
+                flag = True
+
+        if flag:
+            unclean.append(ref)
+
+    if len(unclean) == 0:
+        print("\nAll variables are filtered")
+    else:
+        print("\nunclean variables: ", unclean)
